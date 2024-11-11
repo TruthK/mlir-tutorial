d1e48759533ca26b130f758e6f75ba19802680f0
tk <80590158@qq.com>
2024-11-11 18:08:00 +0800

    td 2

--- lib/Dialect/Ploy/PolyDialect.cpp
+++ lib/Dialect/Ploy/PolyDialect.cpp
@@ -1,5 +1,16 @@
-#pragma once 
-
-#include "mlir/IR/DialectImplementation.h"
-
+#pragma once
 #include "PolyDialect.h"
+
+#include "PolyTypes.h"
+#include "lib/Dialect/Ploy/PolyDialect.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Ploy/PolyTypes.cpp.inc"
+
+namespace mlir::tutorial::poly {
+    void PolyDialect::initialize(){
+        addTypes<
+        # define GET_TYPEDEF_LIST
+        #include "lib/Dialect/Ploy/PolyTypes.cpp.inc"
+        >();
+    }
+}
--- lib/Dialect/Ploy/PolyDialect.h
+++ lib/Dialect/Ploy/PolyDialect.h
@@ -1,5 +1,6 @@
 #pragma once 
 
+
 #include "mlir/IR/DialectImplementation.h"
 
 #include "lib/Dialect/Ploy/PolyDialect.h.inc"
