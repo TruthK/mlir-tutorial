4f4c18914e1c05da96ff541386eaa5f75491b305
tk <80590158@qq.com>
2024-11-12 23:48:03 +0800

    ops

--- lib/Dialect/Poly/PolyOps.cpp
+++ lib/Dialect/Poly/PolyOps.cpp
@@ -2,10 +2,91 @@
 #include "mlir/Dialect/CommonFolders.h"
 #include "mlir/Dialect/Complex/IR/Complex.h"
 #include "mlir/IR/PatternMatch.h"
+#include "llvm/ADT/APInt.h"
 
 #define GET_OP_CLASSES
 #include "lib/Dialect/Poly/PolyOps.cpp.inc"
-
+using namespace llvm;
 namespace mlir::tutorial::poly {
-    // OpFoldResult 
+
+OpFoldResult AddOp::fold(AddOp::FoldAdaptor adaptor) {
+  return constFoldBinaryOp<IntegerAttr, APInt, void>(
+      adaptor.getOperands(), [&](APInt lhs, APInt rhs) { return lhs + rhs; });
 }
+
+OpFoldResult SubOp::fold(SubOp::FoldAdaptor adaptor) {
+  return constFoldBinaryOp<IntegerAttr, APInt, void>(
+      adaptor.getOperands(), [&](APInt lhs, APInt rhs) { return lhs - rhs; });
+}
+
+OpFoldResult MulOp::fold(MulOp::FoldAdaptor adaptor) {
+  auto lhs = dyn_cast_or_null<DenseIntElementsAttr>(adaptor.getOperands()[0]);
+  auto rhs = dyn_cast_or_null<DenseIntElementsAttr>(adaptor.getOperands()[1]);
+
+  if (!lhs || !rhs)
+    return nullptr;
+
+  auto degree =
+      llvm::cast<PolynomialType>(getResult().getType()).getDegreeBound();
+  auto maxIndex = lhs.size() + rhs.size() - 1;
+
+  SmallVector<APInt, 8> result;
+  result.reserve(maxIndex);
+  for (int i = 0; i < maxIndex; i++) {
+    result.push_back(APInt((*lhs.begin()).getBitWidth(), 0));
+  }
+
+  int i = 0;
+  for (auto lhsIt = lhs.value_begin<APInt>(); lhsIt != lhs.value_end<APInt>();
+       ++lhsIt) {
+    int j = 0;
+    for (auto rhsIt = rhs.value_begin<APInt>(); rhsIt != rhs.value_end<APInt>();
+         ++rhsIt) {
+      // index is modulo degree because poly's semantics are defined modulo x^N
+      // = 1.
+      result[(i + j) % degree] += *rhsIt * (*lhsIt);
+      ++j;
+    }
+    ++i;
+  }
+
+  return DenseIntElementsAttr::get(
+      RankedTensorType::get(static_cast<int64_t>(result.size()),
+                            IntegerType::get(getContext(), 32)),
+      result);
+}
+
+OpFoldResult ConstantOp::fold(ConstantOp::FoldAdaptor adaptor) {
+  return adaptor.getCoefficients();
+}
+
+OpFoldResult FromTensorOp::fold(FromTensorOp::FoldAdaptor adaptor) {
+  // Returns null if the cast failed, which corresponds to a failed fold.
+  return dyn_cast_or_null<DenseIntElementsAttr>(adaptor.getInput());
+}
+
+LogicalResult EvalOp::verify() {
+  auto pointTy = getPoint().getType();
+  bool isSignlessInteger = pointTy.isSignlessInteger(32);
+  auto complexPt = dyn_cast<ComplexType>(pointTy);
+  return isSignlessInteger || complexPt
+             ? success()
+             : emitOpError("argument point must be a 32-bit "
+                           "integer, or a complex number");
+}
+void AddOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
+                                        ::mlir::MLIRContext *context) {}
+
+void SubOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
+                                        ::mlir::MLIRContext *context) {
+  results.add<DifferenceOfSquares>(context);
+}
+
+void MulOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
+                                        ::mlir::MLIRContext *context) {}
+
+void EvalOp::getCanonicalizationPatterns(::mlir::RewritePatternSet &results,
+                                         ::mlir::MLIRContext *context) {
+  results.add<LiftConjThroughEval>(context);
+}
+} // namespace mlir::tutorial::poly
